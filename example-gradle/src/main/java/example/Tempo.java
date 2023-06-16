0a7136aa335c316c73b0978bd17cbcb404a5badf
Ned Twigg <ned.twigg@diffplug.com>
2023-06-16 10:06:08 -0700

    Move the gradle stuff into its own folder.

---
+++ example-gradle/src/main/java/example/Tempo.java
@@ -0,0 +1,13 @@
+package example;
+
+public class Tempo {
+    private final int bpm;
+
+    public Tempo(int bpm) {
+        this.bpm = bpm;
+    }
+
+    public int getBpm() {
+        return bpm;
+    }
+}
